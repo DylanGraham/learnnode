9c427c6c24884b3400d5071f6fd4f238a9270d25
Dylan Graham <dylan.graham@gmail.com>
2017-02-08 11:38:30 +1100

    rxjs FTW!

--- learn08.js
+++ learn08.js
@@ -1,18 +1,26 @@
 var Rx = require('rxjs/Rx')
 const http = require('http')
-//const url = process.argv[2] 
-var url = 'http://google.com.au/'
-var source = Rx.Observable.bindCallback(httpGet(url))
+const url = process.argv[2]
+var count = 0
+var storage = ''
 
-function cb(response) {
-    response.setEncoding('utf8')
-    response.on("data", (data) => {
-        console.log(data)
-    })
-}
+// Wrap the http.get method
+var get = Rx.Observable.bindCallback(http.get)
+var source = get(url)
 
-
-function httpGet(url) {
-  return http.get(url, cb)
-}
+var sub1 = source.subscribe(
+    response => {
+        response.setEncoding('utf8')
+//        console.log('onNext: %s', response)
+        response.on("data", (data) => {
+            count += data.length
+            storage += data
+        })
+        response.on('end', () => {
+            console.log(count)
+            console.log(storage)
+        })
+    },
+    e => console.log('onError: %s', e))
+//    () => console.log('onCompleted'));
 
