380a992a3107fcd25a2922289f902a3e989bc865
Dylan Graham <dylan.graham@gmail.com>
2017-02-09 19:41:19 +1100

    Passed

--- learn09.js
+++ learn09.js
@@ -1,9 +1,11 @@
-var Rx = require('rxjs/Rx')
+const Rx = require('rxjs/Rx')
 const http = require('http')
+const concat = require('concat-stream')
 const url1 = process.argv[2]
 const url2 = process.argv[3]
 const url3 = process.argv[4]
-var storage = ''
+var count = 0
+var storage = []
 
 // Wrap the http.get method
 var get = Rx.Observable.bindCallback(http.get)
@@ -15,14 +17,25 @@ var source = Rx.Observable.merge(source1, source2, source3)
 
 var sub1 = source.subscribe(
     response => {
-//        console.log('onNext: %s', response)
         response.setEncoding('utf8')
-        response.on("data", (data) => {
-            storage += data
-        })
+        response.pipe(concat( (data) => {
+            storage[count] = data
+            ++count
+            if (count === 3) printAll()
+        }))
+        // response.on("data", (data) => {
+        //     // console.log('on data')
+        //     // storage[count] += data
+        // })
         response.on('end', () => {
-            console.log(storage)
+            // console.log(storage[count])
+            // ++count
         })
     },
     e => console.log('onError: %s', e))
 
+function printAll(){
+    storage.forEach( (data) => {
+        console.log(data)
+    });
+}
