8e6a16333b6ad7ae915d5a0014946ef9d1f96ba0
Dylan Graham <dylan.graham@gmail.com>
2017-02-10 16:37:27 +1100

    es6

--- learn09.js
+++ learn09.js
@@ -1,33 +1,33 @@
-const Rx = require('rxjs/Rx')
-const http = require('http')
-const concat = require('concat-stream')
-const url1 = process.argv[2]
-const url2 = process.argv[3]
-const url3 = process.argv[4]
-var count = 0
-var storage = []
+const Rx = require('rxjs/Rx');
+const http = require('http');
+const concat = require('concat-stream');
+const url1 = process.argv[2];
+const url2 = process.argv[3];
+const url3 = process.argv[4];
+let count = 0;
+let storage = [];
 
 // Wrap the http.get method
-var get = Rx.Observable.bindCallback(http.get)
-var source1 = get(url1)
-var source2 = get(url2)
-var source3 = get(url3)
+let get = Rx.Observable.bindCallback(http.get);
+let source1 = get(url1);
+let source2 = get(url2);
+let source3 = get(url3);
 
-var source = Rx.Observable.merge(source1, source2, source3)
+let source = Rx.Observable.merge(source1, source2, source3);
 
-var sub1 = source.subscribe(
+let sub1 = source.subscribe(
     response => {
-        response.setEncoding('utf8')
-        response.pipe(concat( (data) => {
-            storage[count] = data
-            ++count
+        response.setEncoding('utf8');
+        response.pipe(concat((data) => {
+            storage[count] = data;
+            ++count;
             if (count === 3) printAll()
         }))
     },
-    e => console.log('onError: %s', e))
+    e => console.log('onError: %s', e));
 
 function printAll(){
     storage.forEach( (data) => {
-        console.log(data)
-    });
+        console.log(data);
+    })
 }
