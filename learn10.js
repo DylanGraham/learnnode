5c8bec28e247ee10f9d73bb536f6be02d06c68c3
Dylan Graham <dylan.graham@gmail.com>
2017-02-16 16:53:51 +1100

    passed

---
+++ learn10.js
@@ -0,0 +1,9 @@
+var net = require('net');
+var strftime = require('strftime');
+var port = process.argv[2];
+var server = net.createServer(function (socket) {
+    var date = strftime('%F %R');
+    socket.write(date + "\n");
+    socket.end();
+});
+server.listen(port);
--- learn10.ts
+++ learn10.ts
@@ -1,8 +1,11 @@
 const net = require('net');
+const strftime = require('strftime');
 const port = process.argv[2];
 
 let server = net.createServer(socket => {
-    console.log(socket)
+    const date = strftime('%F %R');
+    socket.write(date + "\n");
+    socket.end();
 });
 
 server.listen(port);
