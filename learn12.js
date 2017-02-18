a4a1952ef2c31cfb237653678c1be4aa5e4a44c9
Dylan Graham <dylan.graham@gmail.com>
2017-02-18 14:55:58 +1100

    Added check for non POST requests

--- learn12.js
+++ learn12.js
@@ -2,6 +2,9 @@ var http = require('http');
 var t2map = require('through2-map');
 var port = process.argv[2];
 var server = http.createServer(function (req, res) {
+    if (req.method !== 'POST') {
+        return res.end('send me a POST\n');
+    }
     req.pipe(t2map(function (chunk) {
         return chunk.toString().toUpperCase();
     })).pipe(res);
--- learn12.ts
+++ learn12.ts
@@ -3,6 +3,10 @@ const t2map = require('through2-map');
 const port = process.argv[2];
 
 const server = http.createServer((req, res) => {
+    if (req.method !== 'POST') {
+        return res.end('send me a POST\n');
+    }
+
     req.pipe(t2map(chunk => {
         return chunk.toString().toUpperCase();
     })).pipe(res);
